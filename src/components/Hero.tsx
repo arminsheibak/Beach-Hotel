bd25a229c9f06100254890a8f28e64fa1b59e7d1
Armin <armin2003sheibak@gmail.com>
2024-08-04 12:53:49 +0330

    made the hero background dynamic

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,12 @@ import { ReactNode } from "react";
 
 interface Props {
   heroClass?: string;
+  backgroundImageUrl?: string
   children: ReactNode;
 }
 
-const Hero = ({ heroClass='defaultHero', children }: Props) => {
-  return <header className={heroClass}>{children}</header>;
+const Hero = ({ heroClass='defaultHero', children, backgroundImageUrl }: Props) => {
+  return <header style={backgroundImageUrl ? {backgroundImage: `url("${backgroundImageUrl}")`} : {}} className={heroClass}>{children}</header>;
 };
 
 export default Hero;
--- src/pages/RoomDetail.tsx
+++ src/pages/RoomDetail.tsx
@@ -1,7 +1,30 @@
+import { useContext } from "react";
+import { Link, useParams } from "react-router-dom"
+import RoomsContext from "../contexts/roomsContext";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 const RoomDetail = () => {
+  const {rooms} = useContext(RoomsContext)
+  const {slug} = useParams();
+  const room = rooms.find(room => room.fields.slug === slug)
+
+  if (!room) {
+    return (
+      <Hero>
+      <Banner title="404" subtitle="Room not Found" >
+        <Link to='/rooms' className="btn-primary" >Back to Rooms</Link>
+      </Banner>
+    </Hero>
+    )
+  }
+
   return (
-    <div>RoomDetail</div>
+    <Hero heroClass="roomsHero" backgroundImageUrl={room.fields.images[0].fields.file.url} >
+      <Banner title={room.fields.name}>
+      <Link to='/rooms' className="btn-primary" >Back to Rooms</Link>
+      </Banner>
+    </Hero>
   )
 }
 
