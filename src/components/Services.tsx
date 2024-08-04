4a16217ac04e36bb946dff6dcdde02918d97db05
Armin <armin2003sheibak@gmail.com>
2024-08-04 10:21:36 +0330

    build hotel services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,33 @@
+import SectionTitle from "./SectionTitle"
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa'
+
+interface Service {
+  icon: any;
+  title: string;
+  info: string;
+}
 
 const Services = () => {
+  const services: Service[] = [
+    { icon: <FaCocktail /> , title:'Free Cocktails' , info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut!'},
+    { icon: <FaHiking />  , title: 'Endless Hiking'  , info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut!'},
+    { icon:  <FaShuttleVan /> , title: 'Free Shuttle'  , info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut!'},
+    { icon: <FaBeer /> , title: 'Strongest Beer'  , info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, ut!'},
+  ]
+
   return (
-    <div>Services</div>
+    <section className="services" >
+    <SectionTitle title="Services" />
+    <div className="services-center">
+      {services.map(service => {
+        return <article key={service.title} className="service" >
+          <span>{service.icon}</span>
+          <h6>{service.title}</h6>
+          <p>{service.info}</p>
+        </article>
+      } )}
+    </div>
+    </section>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom"
 import Hero from "../components/Hero"
 import Banner from "../components/Banner"
+import Services from "../components/Services"
 
 const HomePage = () => {
   return (
+    <>
     <Hero>
       <Banner title='Luxurious Rooms' subtitle='Deluxe rooms starting at $299' >
         <Link to='rooms/' className="btn-primary" >our rooms</Link>
       </Banner>
     </Hero>
+    <Services></Services>
+    </>
+    
   )
 }
 
