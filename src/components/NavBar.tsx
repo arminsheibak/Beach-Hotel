b80a0be4e86efe941fd3986a5d990b26fc4956e4
Armin <armin2003sheibak@gmail.com>
2024-08-04 09:24:11 +0330

    build navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+import logo from '../images/logo.svg'
+import { FaAlignRight } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
 
 const NavBar = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <div>NavBar</div>
+    <nav className='navbar' >
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to='/' ><img src={logo} alt="" /></Link>
+          <button type='button'  className='nav-btn' onClick={() => setOpen(!isOpen)} >
+            <FaAlignRight className='nav-icon' />
+          </button>
+        </div>
+        <ul className={isOpen ? 'nav-links show-nav' : 'nav-links'} >
+          <li><Link to='/' >Home</Link></li>
+          <li><Link to='/rooms' >Rooms</Link></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
